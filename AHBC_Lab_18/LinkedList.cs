de6f600b0def21e70dee83a9dcbfdac064ca8cd2
swurst954 <swurst6@gmail.com>
2018-12-10 16:15:59 -0500

    Lab 18

---
+++ AHBC_Lab_18/LinkedList.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class LinkedList
+{
+    public static bool InsertAt<T>(this LinkedList<T> list, int index, T o)
+    {
+        var count = list.Count;
+        var boolValue = false;
+
+        if (index >= count)
+        {
+            boolValue = false;
+        }
+        else
+        {
+            for (int i = 0; i < count; i++)
+            {
+                if (i == index)
+                {
+                    list.AddLast(o);
+                }
+                list.AddLast(list.ElementAt(i));
+            }
+            for (int i = 0; i < count; i++)
+            {
+                list.RemoveFirst();
+            }
+            boolValue = true;
+        }
+        return boolValue;
+    }
+
+    public static bool RemoveAt<T>(this LinkedList<T> list, int index)
+    {
+        var count = list.Count;
+        var boolValue = false;
+        if (index >= count)
+        {
+            boolValue = false;
+        }
+        else
+        {
+            for (int i = 0; i < count; i++)
+            {
+                if (i != index)
+                {
+                    list.AddLast(list.ElementAt(i));
+                }
+            }
+            for (int i = 0; i < count; i++)
+            {
+                list.RemoveFirst();
+            }
+            boolValue = true;
+        }
+        return boolValue;
+    }
+
+    public static void PrintReverse<T>(this LinkedList<T> list)
+    {
+        for (int i = list.Count - 1; i >= 0; i--)
+        {
+            Console.WriteLine(list.ElementAt(i));
+        }
+    }
+}
---
+++ AHBC_Lab_18/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AHBC_Lab_18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //1
+            LinkedList<int> list = new LinkedList<int>();
+
+            list.AddFirst(16);
+            list.AddLast(27);
+            list.AddLast(8);
+            list.AddLast(1);
+            list.AddLast(4);
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list.ElementAt(i));
+            }
+            Console.WriteLine("---------------------");
+
+            bool indexBool = list.RemoveAt(8);
+
+            Console.WriteLine(indexBool);
+            Console.WriteLine("---------------------");
+
+            list.PrintReverse();
+
+
+            //2
+            int[] numArray = { 1, 2, 3, 2, 2, 4, 5, 5, 7, 8, 4, 4, 1, 0, 10 };
+            Console.WriteLine("---------------------");
+
+            Frequency(numArray);
+            Console.WriteLine("---------------------");
+            DictionaryFreq(numArray);
+
+            Console.ReadKey();
+        }
+
+        public static void Frequency(int[] numberArray)
+        {
+            int arrayCount = numberArray.Max();
+
+            int[] arrayFreq = new int[arrayCount + 1];
+
+            for (int i = 0; i < arrayFreq.Length; i++)
+            {
+                for (int j = 0; j < numberArray.Length; j++)
+                {
+                    if (numberArray[j] == i)
+                    {
+                        arrayFreq[i]++;
+                    }
+                }
+            }
+            for (int i = 0; i < arrayFreq.Length; i++)
+            {
+                if (arrayFreq[i] > 0)
+                {
+                    Console.WriteLine($"[{i}]: {arrayFreq[i]}");
+                }
+            }
+        }
+
+        public static void DictionaryFreq(int[] numberArray)
+        {
+            Dictionary<int, int> list = new Dictionary<int, int>();
+
+            foreach (int number in numberArray)
+            {
+                if (list.ContainsKey(number))
+                {
+                    list[number]++;
+                }
+                else
+                {
+                    list.Add(number, 1);
+                }
+            }
+            foreach (var item in list.OrderBy(key => key.Key))
+            {
+                Console.WriteLine($"[{item.Key}]: {item.Value}");
+            }
+        }
+    }
+}
